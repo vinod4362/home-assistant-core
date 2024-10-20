7ce568f1756b16f66e54471cdc3425b9f1a45f96
MansStudentBTH <70517420+MansStudentBTH@users.noreply.github.com>
2024-10-20 17:47:25 +0200

    Removed other codesmell fix from this branch (#71)

--- homeassistant/components/wled/update.py
+++ homeassistant/components/wled/update.py
@@ -75,18 +75,21 @@ class WLEDUpdateEntity(WLEDEntity, UpdateEntity):
         """Latest version available for install."""
         # If we already run a pre-release, we consider being on the beta channel.
         # Offer beta version upgrade, unless stable is newer
+        beta = self.releases_coordinator.data.beta
+        current = self.coordinator.data.info.version
+        stable = self.releases_coordinator.data.stable
         if (
-            (beta := self.releases_coordinator.data.beta) is not None
-            and (current := self.coordinator.data.info.version) is not None
+            beta is not None
+            and current is not None
             and (current.alpha or current.beta or current.release_candidate)
             and (
-                (stable := self.releases_coordinator.data.stable) is None
+                stable is None
                 or (stable is not None and stable < beta and current > stable)
             )
         ):
             return str(beta)
 
-        if (stable := self.releases_coordinator.data.stable) is not None:
+        if stable is not None:
             return str(stable)
 
         return None
